38bda26903dbb2bd39be6fc593df5d00db54bd3a
N <dummy.42.user@gmail.com>
2017-03-20 14:47:19 +0200

    -- Made some changes for Step 2 (Contact page).

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -22,31 +22,29 @@ class PageController extends Controller {
   public function contactAction(Request $request) {
     $enquiry = new Enquiry();
 
-    $form = $this->createForm(EnquiryType::class, $enquiry);
+    $form = $this
+      ->createForm(EnquiryType::class, $enquiry)
+      ->handleRequest($request);
 
-    if ($request->isMethod($request::METHOD_POST)) {
-      $form->handleRequest($request);
-
-      if ($form->isValid()) {
-        $message = \Swift_Message::newInstance()
-          ->setSubject('Contact enquiry from symblog')
-          ->setFrom('enquiries@symblog.co.uk')
-          ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
-          ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+    if ($form->isValid()) {
+      $message = \Swift_Message::newInstance()
+        ->setSubject('Contact enquiry from symblog')
+        ->setFrom('symfony-blog@sc-n.ua')
+        ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+        ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
 
 
-        $this->get('mailer')->send($message);
+      $this->get('mailer')->send($message);
 
-        $this->get('session')
-          ->getFlashBag()
-          ->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+      $this->get('session')
+        ->getFlashBag()
+        ->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
 
-        // Redirect - This is important to prevent users re-posting
-        // the form if they refresh the page
-        return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+      // Redirect - This is important to prevent users re-posting
+      // the form if they refresh the page
+      return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
 //        $this->redirectToRoute('BloggerBlogBundle_contact');
 
-      }
     }
 
     return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
